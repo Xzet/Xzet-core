933452baa364505d804dbb1209cc2c703fcc26b2
YJSoft <yjsoft@yjsoft.pe.kr>
2014-03-30 11:09:14 +0900

    들여쓰기 수정

--- config/config.inc.php
+++ config/config.inc.php
@@ -5,14 +5,14 @@
      * @brief  기본적으로 사용하는 class파일의 include 및 환경 설정을 함
      **/
 
-if(version_compare(PHP_VERSION, '5.4.0', '<'))
-{
-	@error_reporting(E_ALL ^ E_NOTICE ^ E_DEPRECATED ^ E_WARNING);
-}
-else
-{
-	@error_reporting(E_ALL ^ E_NOTICE ^ E_DEPRECATED ^ E_WARNING ^ E_STRICT);
-}
+    if(version_compare(PHP_VERSION, '5.4.0', '<'))
+    {
+        @error_reporting(E_ALL ^ E_NOTICE ^ E_DEPRECATED ^ E_WARNING);
+    }
+    else
+    {
+        @error_reporting(E_ALL ^ E_NOTICE ^ E_DEPRECATED ^ E_WARNING ^ E_STRICT);
+    }
 
     if(!defined('__ZBXE__')) exit();
 
