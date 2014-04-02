2f6f655ab37c50d72f88732f3c719698782c7256
YJSoft <yjsoft@yjsoft.pe.kr>
2014-04-02 11:34:34 +0900

    for developers

--- config/config.inc.php
+++ config/config.inc.php
@@ -11,7 +11,7 @@ if(version_compare(PHP_VERSION, '5.4.0', '<'))
 }
 else
 {
-	@error_reporting(E_ALL ^ E_NOTICE ^ E_DEPRECATED ^ E_WARNING ^ E_STRICT);
+	@error_reporting(E_ALL ^ E_NOTICE ^ E_DEPRECATED ^ E_STRICT);
 	//@error_reporting(E_ALL ^ E_NOTICE ^ E_DEPRECATED ^ E_STRICT);
 }
 
